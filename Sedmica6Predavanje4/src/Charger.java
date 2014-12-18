9420e9735ee6563b501d8dbc7b3a0a31d3c7c57b
nvuchinic <nermin.vucinic@gmail.com>
2014-12-18 11:16:02 +0100

    Create Charger.java

---
+++ Sedmica6Predavanje4/src/Charger.java
@@ -0,0 +1,6 @@
+public class Charger  extends Zombi{
+	public Charger(String name, double hp, double dex, Weapon wpn, double strenght, double speed){
+		super("Charger",  hp, dex, wpn, strenght,  speed);
+	}
+
+}
