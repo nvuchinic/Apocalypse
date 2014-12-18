d353c6b48d623e65309262bf96a85918a3fd5553
nvuchinic <nermin.vucinic@gmail.com>
2014-12-18 11:17:25 +0100

    Create Zombi.java

---
+++ Sedmica6Predavanje4/src/Zombi.java
@@ -0,0 +1,6 @@
+public class Zombi extends Character {
+public Zombi(String name, double hp, double dex, Weapon wpn, double strenght, double speed){
+	super(name,  hp, dex, wpn, strenght,  speed);
+}
+	
+}
