11e201b3ee6ca07d75d085c1caf87bc2189324c0
nvuchinic <nermin.vucinic@gmail.com>
2014-12-18 11:23:44 +0100

    Create Jockey.java

---
+++ Sedmica6Predavanje4/src/Jockey.java
@@ -0,0 +1,5 @@
+public class Jockey extends Zombi{
+	public Jockey(String name, double hp, double dex, Weapon wpn, double strenght, double speed){
+		super("Jockey",  hp, dex, wpn, strenght,  speed);
+	}
+}
