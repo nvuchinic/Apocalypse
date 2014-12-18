9fafa89f1e513f7408b7d8753d850bffec8dc76e
nvuchinic <nermin.vucinic@gmail.com>
2014-12-18 11:16:31 +0100

    Create Spitter.java

---
+++ Sedmica6Predavanje4/src/Spitter.java
@@ -0,0 +1,5 @@
+public class Spitter extends Zombi{
+public Spitter(String name, double hp, double dex, Weapon wpn, double strenght, double speed){
+	super("Spitter",  hp, dex, wpn, strenght,  speed);
+}
+}
